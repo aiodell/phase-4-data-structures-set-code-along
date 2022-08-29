ae9ad7e7ba84ebd030e4063fb464161c1bd03a02
Andrianna <andrianna.odell@gmail.com>
2022-08-29 17:25:24 -0400

    commit tree e0fcaa8d799a7f6b26263b4dca3908cdef250771

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,32 @@
-# your code here
+class MySet
+
+	def initialize(enumerable = []) # accounts for the two cases
+		# store the values passed in as keys on the Hash
+		@hash = {}
+		enumerable.each do |value|
+			@hash[value] = true
+		end
+	end
+
+	def include?(value)
+  	@hash.has_key?(value)
+	end
+
+	def add(value)
+		@hash[value] = true # add a value as a key on the hash
+		self                # return the updated set
+	end
+
+	def delete(value)
+  	@hash.delete(value)
+  	self
+	end
+
+	def size
+  	@hash.size
+	end
+
+end
+
+set = MySet.new
+set = MySet.new([1,2,3,3])
